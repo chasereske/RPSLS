1b1f1f6d712816a502678262f6ae74662b5de6ec
chasereske <chasereske@gmail.com>
2021-02-02 20:10:46 -0500

    I added the game class and built in the rules that should be displayed at the beginning of the game. I also built in the player class, the human player class that extends player, and the computer class that extends player. I will need to further develop the player classes.

---
+++ game.js
@@ -0,0 +1,43 @@
+"use strict"
+
+//two player game
+//single player is human and AI and multiplayer is human and human
+//store gestures in an array and utilize the array to display and assign gestures
+//the correct player needs to win a given round based on the choices made by each player
+//needs to be a best of 3 to decide winner
+
+//construct a Game class and use this to be the parent for single and multiplayer games
+class Game {
+    constructor() {
+        this.playerOne //build out if human or AI
+        this.playerTwo //build out if human or AI
+    }
+
+    displayRules() {
+        console.log("Welcome to Rock-Paper-Scissors-Lizard-Spock Game!");
+        console.log("Two players will compete by choosing gestures.");
+        console.log("Rock crushes Scissors.\nScissors cuts Paper.\nPaper covers Rock.\nRock crushes Lizard.\nLizard poisons Spock.\nSpock smashes Scissors.\nScissors decapitates Lizard.\nLizard eats Paper.\nPaper disproves Spock.\nSpock vaporizes Rock.");
+        console.log("The winner of RPSLS will, at a minimum, need to win the best of three games to be crowned the victor!");
+    }
+
+    class Player {
+        constructor() {
+            this.score = 0;
+        }
+    }
+
+    class HumanPlayer extends Player {
+        constructor(name) {
+            super(score);
+            this.name = name;
+        }
+    }
+
+    class ComputerPlayer extends Player {
+        constructor() {
+            super(score);
+            this.name = "Computer";
+        }
+    }
+}
+
